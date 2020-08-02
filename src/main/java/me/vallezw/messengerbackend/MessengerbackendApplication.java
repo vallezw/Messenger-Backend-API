28a713118e978bea27c95aa2ffbb8907e12c7faa
Valentin Zwerschke <v.zwerschke@mail.de>
2020-08-02 13:36:48 +0000

    Create Spring Boot application via SpringInitializr

---
+++ src/main/java/me/vallezw/messengerbackend/MessengerbackendApplication.java
@@ -0,0 +1,13 @@
+package me.vallezw.messengerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerbackendApplication.class, args);
+    }
+
+}
