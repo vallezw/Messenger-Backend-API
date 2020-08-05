722ca3d8a95bbd44a57029315bc060983d2c9826
Valentin Zwerschke <v.zwerschke@mail.de>
2020-08-05 10:45:31 +0000

    Add getting token in controller

--- src/main/java/me/vallezw/messengerbackend/HelloResource.java
+++ src/main/java/me/vallezw/messengerbackend/HelloResource.java
@@ -2,15 +2,14 @@ package me.vallezw.messengerbackend;
 
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
 
     @RequestMapping("/hello")
-    public String hello() {
-
-        return "Hello World";
+    public String hello(@RequestHeader (name="Authorization") String header) {
+        String token = header.substring(7);
+        return token;
     }
 }
