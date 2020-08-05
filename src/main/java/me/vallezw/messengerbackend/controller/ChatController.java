2a379d516e0b2688dbbbbd24207f71ec3149c1dd
Valentin Zwerschke <v.zwerschke@mail.de>
2020-08-05 10:57:27 +0000

    Create getChats endpoint

--- src/main/java/me/vallezw/messengerbackend/controller/ChatController.java
+++ src/main/java/me/vallezw/messengerbackend/controller/ChatController.java
@@ -1,4 +1,32 @@
 package me.vallezw.messengerbackend.controller;
 
+import me.vallezw.messengerbackend.authentication.util.JwtUtil;
+import me.vallezw.messengerbackend.database.chats.Chat;
+import me.vallezw.messengerbackend.database.chats.ChatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ChatController {
+
+    @Autowired
+    ChatRepository chatRepository;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+
+    @RequestMapping("/getChats")
+    public List<Chat> getChats(@RequestHeader (name="Authorization") String header) {
+        String token = header.substring(7);
+        String username = jwtUtil.extractUsername(token);
+        List<Chat> chats = chatRepository.findByUser1OrUser2(username, username);
+        return chats;
+    }
+    
 }
--- src/main/java/me/vallezw/messengerbackend/database/chats/ChatRepository.java
+++ src/main/java/me/vallezw/messengerbackend/database/chats/ChatRepository.java
@@ -2,6 +2,10 @@ package me.vallezw.messengerbackend.database.chats;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 public interface ChatRepository extends CrudRepository<Chat, Long> {
+    public List<Chat> findByUser1OrUser2(String user1, String user2);
+
 }
