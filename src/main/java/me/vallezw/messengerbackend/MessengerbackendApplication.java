d50bde0ae5aca26fa49d2f4ddb7a8633296b7a47
Valentin Zwerschke <v.zwerschke@mail.de>
2020-08-05 12:47:53 +0000

    Deleted arraylist

--- src/main/java/me/vallezw/messengerbackend/MessengerbackendApplication.java
+++ src/main/java/me/vallezw/messengerbackend/MessengerbackendApplication.java
@@ -3,9 +3,10 @@ package me.vallezw.messengerbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// MyText
+
 @SpringBootApplication
 public class MessengerbackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MessengerbackendApplication.class, args);
     }
--- src/main/java/me/vallezw/messengerbackend/chats/controller/ChatController.java
+++ src/main/java/me/vallezw/messengerbackend/chats/controller/ChatController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
