bf131d0dc656bf6c6151ae015f0f26c8a257c431
Valentin Zwerschke <v.zwerschke@mail.de>
2020-08-05 10:48:52 +0000

    Add getting username within the method

--- src/main/java/me/vallezw/messengerbackend/HelloResource.java
+++ src/main/java/me/vallezw/messengerbackend/HelloResource.java
@@ -1,5 +1,7 @@
 package me.vallezw.messengerbackend;
 
+import me.vallezw.messengerbackend.authentication.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloResource {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @RequestMapping("/hello")
     public String hello(@RequestHeader (name="Authorization") String header) {
         String token = header.substring(7);
-        return token;
+        String username = jwtUtil.extractUsername(token);
+        return "Hello " + username;
     }
 }
