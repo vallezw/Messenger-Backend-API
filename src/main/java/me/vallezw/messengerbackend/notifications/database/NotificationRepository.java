3c12949f19b1d7adddfdd93fcc98c6934c8c6daa
Valentin <v.zwerschke@mail.de>
2020-08-22 18:16:17 +0000

    Make changes in NotifiactionRepository

--- src/main/java/me/vallezw/messengerbackend/notifications/database/NotificationRepository.java
+++ src/main/java/me/vallezw/messengerbackend/notifications/database/NotificationRepository.java
@@ -3,5 +3,4 @@ package me.vallezw.messengerbackend.notifications.database;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
-
 }
